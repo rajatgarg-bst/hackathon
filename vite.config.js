f53fa9300a92a5c9d7feb2fe176057377cb7b662
rajatgarg-bst <60380108+rajatgarg-bst@users.noreply.github.com>
2025-03-29 01:23:14 +0530

    fix: correct production mode check in Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: import.meta.env.PROD ? "/hackathon/" : "/",
+  base: command === "build" ? "/hackathon/" : "/",
   publicDir: "public",
   build: {
     assetsDir: "assets",
   },
-});
+}));
