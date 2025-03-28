8d1122d480aba25f9e00b10468b6dd319189fb84
rajatgarg-bst <60380108+rajatgarg-bst@users.noreply.github.com>
2025-03-29 01:41:31 +0530

    Fix dependency versions and Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === "build" ? "/hackathon/" : "/",
+  base: "/",
   publicDir: "public",
   build: {
     assetsDir: "assets",
   },
-}));
+});
