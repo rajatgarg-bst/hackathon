693fd83ffd0f3d66ae53edacf0914beac3806267
rajatgarg-bst <60380108+rajatgarg-bst@users.noreply.github.com>
2025-03-29 01:22:40 +0530

    fix: make base URL conditional for development and production

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hackathon/",
+  base: import.meta.env.PROD ? "/hackathon/" : "/",
   publicDir: "public",
   build: {
     assetsDir: "assets",
